1893b381550aa681d153f4de91dc7b5847ef9409
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-10-15 00:56:36 +0200

    Änderungen am Programm Zuweisungsoperatoren

--- G_Zuweisungsoperatoren/main.c
+++ G_Zuweisungsoperatoren/main.c
@@ -8,6 +8,22 @@ int main(void)
 {
 	int x = 10;
 
-	printf("Hello, World!\n");
+	x = x + 2;
+	x += 2;
+
+	// x = x - 3;
+	// x -= 3;
+
+	// x = x * 4;
+	// x *= 4;
+
+	// x = x / 5;
+	// x /= 5;
+
+	// x = x % 2;
+	// x %= 2;
+
+	printf("%d", x);
 	return 0;
 }
+
---
+++ H_User_Input/main.c
@@ -0,0 +1,11 @@
+/*
+ * Programm zur Erläuterung von Benutzereingaben
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, World!\n");
+	return 0;
+}
