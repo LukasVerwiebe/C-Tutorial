090fabfed27736eb3c15124310babace3d809eb6
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-10-14 17:23:01 +0200

    Update main.c

--- C_Variablen/main.c
+++ C_Variablen/main.c
@@ -6,12 +6,12 @@
 
 int main()
 {
-	int x;					// Variable Deklarieren
-	x = 123;				// Variable Initialisieren
-	int y = 321;			// Variablen Deklaration und Initialisierung
+	int x;			// Variable Deklarieren
+	x = 123;		// Variable Initialisieren
+	int y = 321;		// Variablen Deklaration und Initialisierung
 
-	int alter = 21;			// Integer
-	float note = 2.22;		// Fließkommazahl
+	int alter = 21;		// Integer
+	float note = 2.22;	// Fließkommazahl
 	char buchstabe = 'C';	// Einzelnes Zeichen
 	char name[] = "Lukas";	// Zeichenkette
 
