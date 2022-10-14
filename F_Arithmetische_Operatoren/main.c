763b1015365810d117cca90fe5ce37d8575c90ce
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-10-14 22:07:07 +0200

    Änderungen am Programm Arithmetische Operatoren

--- F_Arithmetische_Operatoren/main.c
+++ F_Arithmetische_Operatoren/main.c
@@ -9,6 +9,27 @@ int main(void)
 	int x = 5;
 	int y = 2;
 
-	printf("Hello, World!\n");
+	int a = x + y;
+	int b = x - y;
+	int c = x * y;
+
+	int d = x / y;
+	float e = x / (float)y;
+
+	int f = x % y;
+
+	printf("\n%d", a);
+	printf("\n%d", b);
+	printf("\n%d", c);
+
+	printf("\n%d", d);
+	printf("\n%f", e);
+
+	printf("\n%d", f);
+
+	x++;
+	printf("\n%d", x);
+	x--;
+	printf("\n%d", x);
 	return 0;
 }
