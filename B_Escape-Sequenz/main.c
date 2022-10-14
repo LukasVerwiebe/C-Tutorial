499b6b93f9ec9a1068c18f85b4bc693fb2b052f7
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-10-14 16:41:27 +0200

    Variablen Programm hinzugefügt

--- B_Escape-Sequenz/main.c
+++ B_Escape-Sequenz/main.c
@@ -6,6 +6,16 @@
 
 int main(void)
 {
-	printf("Hello, World!\n");
+	// Neue Zeile mit \n
+	printf("Neue Zeile\n");
+	// Tab mit \t
+	printf("1 \t 2 \t 3 \n4 \t 5 \t 6 \n7 \t 8 \t 9 \n");
+	// Anzeige von "
+	printf("\"Dies ist ein Test\" - Zitat: Lukas \n");
+	// Anzeige von '
+	printf("\'Dies ist ein Test\' - Zitat: Lukas \n");
+	// Anzeige von /
+	printf("\\Dies ist ein Test\\ - Zitat: Lukas \n");
 	return 0;
 }
+
---
+++ C_Variablen/main.c
@@ -0,0 +1,20 @@
+/*
+ * Programm zur Erläuterung von Variablen
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	int x;					// Variable Deklarieren
+	x = 123;				// Variable Initialisieren
+	int y = 321;			// Variablen Deklaration und Initialisierung
+
+	int alter = 21;			// Integer
+	float note = 2.22;		// Fließkommazahl
+	char buchstabe = 'C';	// Einzelnes Zeichen
+	char name[] = "Lukas";	// Zeichenkette
+
+
+	return 0;
+}
