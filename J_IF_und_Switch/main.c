45f401423c25135d9a984af41189a243b67068c1
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-10-15 14:17:37 +0200

    Änderungen am Programm IF_und_Switch

--- J_IF_und_Switch/main.c
+++ J_IF_und_Switch/main.c
@@ -28,5 +28,32 @@ int main(void)
 		printf("Du bist zu jung um dich anzumelden!");
 	}
 
+
+	int note;
+
+	printf("\nGib eine Note ein:");
+	scanf("%d", &note);
+
+	switch(note)
+	{
+	case '1':
+		printf("Sehr gut\n");
+		break;
+	case '2':
+		printf("Gut\n");
+		break;
+	case '3':
+		printf("Befriedigent\n");
+		break;
+	case '4':
+		printf("Ausreichend\n");
+		break;
+	case '5':
+		printf("Mangelhaft\n");
+		break;
+	default:
+		printf("Dabei handelt es sich nicht um eine gültige Note!");
+	}
+
 	return 0;
 }
