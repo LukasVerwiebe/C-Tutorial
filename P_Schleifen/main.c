09187bf3dc8f13bf27650f56ac1beefdf35dcc93
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-10-16 00:45:52 +0200

    Programm Schleifen hinzugefügt

---
+++ P_Schleifen/main.c
@@ -0,0 +1,18 @@
+/*
+ * Programm zur Erläuterung der Funktionsweise von Schleifen
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	// For Schleife
+	for(int i = 10; i >= 1; i-=3)
+	{
+		printf("%d\n", i);
+	}
+
+	
+
+	return 0;
+}
